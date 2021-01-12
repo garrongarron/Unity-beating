813727f5e11f5789ad03fb03ed70cd973b9448db
Federico Zacayan <federico.zacayan@gmail.com>
2021-01-11 21:05:24 -0300

    first commit

---
+++ Assets/Scripts/UI/Beating.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beating : MonoBehaviour
+{
+    RectTransform rectTransform;
+    int n = 0;
+    Vector3 original;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        original = rectTransform.localScale;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rectTransform.localScale += Vector3.one * Mathf.Sin(Time.time);
+        // rectTransform.localScale += new Vector3(Mathf.Sin(Time.time), 0.0f, 0.0f);
+        rectTransform.localScale = original+ Vector3.one * Mathf.Sin(Time.time*8) * 0.1f;
+        Debug.Log(1/Time.deltaTime);
+    }
+}
